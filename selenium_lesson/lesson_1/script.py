74f6d5dae00bbcf3ea02cd102909b7292ea1015d
Anastasiya Zakharova <111999983+Nastya-Leto@users.noreply.github.com>
2025-04-23 22:05:47 +0400

    Добавление ДЗ по занятию по selenium_2

--- selenium_lesson/lesson_1/script.py
+++ selenium_lesson/lesson_1/script.py
@@ -5,6 +5,7 @@ driver = webdriver.Chrome()
 driver.get('https://testautomationpractice.blogspot.com/')
 wikipedia_icon = driver.find_element("class name","wikipedia-icon")
 wikipedia_input_field = driver.find_element("id","Wikipedia1_wikipedia-search-input")
+wikipedia_field_button = driver.find_element("css selector",".wikipedia-search-button")
 button_element = driver.find_element("tag name", "button")
 
 time.sleep(3)
